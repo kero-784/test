1701f6e78896151145b8caddc30fe596fa52d9e7
kero-784 <170969138+kero-784@users.noreply.github.com>
2026-03-01 16:19:20 +0200

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_URL = 'https://ghwtecscpjxkrcrvadkp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdod3RlY3NjcGp4a3JjcnZhZGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNzA0NjYsImV4cCI6MjA4Nzk0NjQ2Nn0.EHkZtsqSOQ8O0ookIKBZMRiL2Bqw_DXz4DgS6N28mzY';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
